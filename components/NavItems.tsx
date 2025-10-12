29207ee142aa2a7d596536fdae81c9b90ecb6f80
Prathin Sajith <prathin@propcrm.com>
2025-10-12 14:44:36 +0400

    Implement Trad Home page widgets

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { NAV_ITEMS } from '@/lib/contants'
+import { NAV_ITEMS } from '@/lib/constants'
 import Link from 'next/link'
 import React from 'react'
 import { usePathname } from 'next/navigation'
--- lib/contants.ts
+++
@@ -1,9 +0,0 @@
-export const NAV_ITEMS = [
-    {href : '/', label: 'Home'},
-    {href : '/search', label: 'Search'},
-    {href : '/watchlist', label: 'Watchlist'},
-    {href : '/blog', label: 'Blog'},
-    {href : '/pricing', label: 'Pricing'},
-    {href : '/login', label: 'Login'},
-    {href : '/signup', label: 'Sign Up'},
-]
